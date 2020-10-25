e8f8ffccdeb87dabe0b3a9c689da9ca8fb2317b2
João Vitor Waldrich <62338614+jvwaldrich0@users.noreply.github.com>
2020-10-25 22:41:51 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-nohup /home/jvwaldrich0/Projects/MyProjects/Linux\ Projects/File-Organizer/main.py &
+nohup /home/jvwaldrich0/Projects/MyProjects/Linux\ Projects/File-Organizer/move.py &
