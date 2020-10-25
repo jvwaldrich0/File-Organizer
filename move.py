35e8d7a8e0d1e717dd489457bf2a18ded43d200b
Joao Vitor Waldrich <jv.waldrich0@protonmail.com>
2020-10-25 20:37:16 +0000

    Remove if file

--- move.py
+++ move.py
@@ -6,15 +6,14 @@ import os
 def move_files(source_folder, target_folder):
     try:
         for path, directory, files in os.walk(source_folder):
-            if files:
-                for file in files:
-                    if not os.path.isfile(target_folder + file):
-                        os.rename(
-                            os.path.join(
-                                path, file),
-                            os.path.join(
-                                target_folder, file)
-                        )
+            for file in files:
+                if not os.path.isfile(target_folder + file):
+                    os.rename(
+                        os.path.join(
+                            path, file),
+                        os.path.join(
+                            target_folder, file)
+                    )
         else:
             print('All files moved')
     except Exception as error:
